24990a7887b4126e1fe60c4af38c9ef6f336cdc2
Halley Sa <127997961+Hallimasa@users.noreply.github.com>
2023-07-19 22:31:46 -0400

    Added new guide, fortuna

--- src/index.js
+++ src/index.js
@@ -337,7 +337,7 @@ client.on('messageCreate', (m) =>{
             })
 
 
-    }  else if (m.content === "config.guiafarm.channel"){   // MENU GUIA FARM
+    } else if (m.content === "config.guiafarm.channel"){   // MENU GUIA FARM
         if (!(m.channel.id === process.env.CHAT_GUIA_FARM_ID)) return;
         const guiaFarm =  new ActionRowBuilder().addComponents(
             new StringSelectMenuBuilder()
@@ -377,7 +377,7 @@ client.on('messageCreate', (m) =>{
             }],
             components: [guiaFarm]
             })
-    }  else if (m.content === "config.guiacetus.channel"){   // MENU GUIA CETUS
+    } else if (m.content === "config.guiacetus.channel"){   // MENU GUIA CETUS
         if (!(m.channel.id === process.env.CHAT_GUIA_CETUS_ID)) return;
         const guiaCetus =  new ActionRowBuilder().addComponents(
             new StringSelectMenuBuilder()
@@ -407,13 +407,46 @@ client.on('messageCreate', (m) =>{
             }],
             components: [guiaCetus]
             })
-    }
+    } else if (m.content === "config.guiafortuna.channel"){   // MENU GUIA FORTUNA
+        if (!(m.channel.id === process.env.CHAT_GUIA_FORTUNA_ID)) return;
+        const guiaFortuna =  new ActionRowBuilder().addComponents(
+            new StringSelectMenuBuilder()
+            .setCustomId('guiaFortuna')
+            .setPlaceholder('Selecione um item')
+            .setMaxValues(1)
+            .setMinValues(1)
+            .setOptions ([
+                {label:'1. Npcs',value:'npcsDeFortuna',description:'Um breve resumo sobre o que cada NPC oferece'},
+                {label:'2. Farm de Reputação',value:'farmDeReputacaoFortuna',description:'Maneiras mais eficientes de farmar Reputação'},
+                {label:'3. Farm de Recursos',value:'farmRecursosFortuna',description:'Maneiras mais eficientes de farmar Foco'},
+                {label:'4. Pescaria & Mineração',value:'pescaria&MineracaoFortuna',description:'Informações básicas sobre pescaria e mineração'},
+                {label:'5. Kitguns - Armas Modulares',value:'kitguns',description:'Guia dessas armas secundárias modulares'},
+                {label:'6. Orb Beneficiária ( Profit Taker )',value:'orbBeneficiaria',description:'Como matar o boss?'},
+                {label:'7. Orb Exploradora',value:'orbExploradora',description:'Como matar esse boss?'},
+                {label:'8. K-drive',value:'kdrive',description:'Como upar meu k-drive?'},
+                {label:'9. Eventos de Fortuna',value:'eventosDeFortuna',description:'Como fazer os eventos de Fortuna?'}
+            ]),
+        );
+    
+        m.channel.send({
+            embeds:[{
+                title:'GUIA FORTUNA',
+                description:`Fortuna é o segundo mapa aberto que foi implementado ao game, chegando no final de 2018. Nele vemos 5 moldes que se repetiram desde Cetus e nos próximos mapas abertos lançados nos anos seguintes, são eles: \n\n- Mapa Aberto - **Orb Vallis**-  ( Local gigante onde o player faz as missões )\n- HUB de NPCS - **Fortuna** - ( Local onde o player interage com os NPCs )\n- Área de Carregamento - **Elevador** -( Uma área de transferência entre o HUB de Npcs e o Mapa Aberto )\n- Jornada Inicial - **Jornada Vigília de Saya** - ( Jornada necessária para poder interagir com os NPCs)\n- Sindicato Princial - **Solaris United** - ( Sindicato onde o player consegue reputação para trocar por itens com os NPCs )\n- Sindicato Secundários - **Vox Solaris** - ( Sindicato Secreto onde o player consegue itens para seu Operador , AMPS & NECRAMECH )\n- Sindicato Secundários - **Ventkids** - ( Sindicato onde voce consegue upar seu Skate, k-Drive )\n\nNesse guia iremos abordar como farmar cada recurso dessse mapa, como subir de rank com o sindicato dos NPCs de forma eficiente, qual a utilidade de cada NPC e outras dicas. Espero que goste, qualqer dúvida/erro ou sugestão ao guia, informar ao kenzouframe :D\n- Última Atualização desse guia ${time(new Date(),'R')}\n- Alterado por kenzouframe `,
+                color: '2123412',
+                thumbnail:{
+                    url:'https://i.imgur.com/2Gf6xg4.png'
+                }
+            }],
+            components: [guiaFortuna]
+            })
+    } 
 
 });
   
 // BUTTONS INTERACTIONS     -----------------------------------
 client.on('interactionCreate', async (inte) => {
     if (!inte.isAnySelectMenu())return;
+    console.log(`📕 gerando guia ${inte.customId} para ${inte.member.user.username} `)
 
     if ((inte.customId === 'guiageralBasico')||(inte.customId === 'guiaGeralIntermediario')||(inte.customId === 'guiaGeralAvancado')){    // GUIA GERAL
 
@@ -567,6 +600,45 @@ client.on('interactionCreate', async (inte) => {
             inte.reply({embeds:[embed[0],embed[1],embed[2],embed[3],embed[4],embed[5],embed[6],embed[7],embed[8],embed[9]], ephemeral : true}).then(msg => setTimeout(() => msg.delete(), 360000)); 
         } else { inte.reply({content:'esse guia possui mais de 9 embeds ... ❌',ephemeral:'true'}).then(msg => setTimeout(() => msg.delete(), 6000))}
     } else { inte.reply({content:'essa interação ainda não está pronta ... 🙄',ephemeral:'true'}).then(msg => setTimeout(() => msg.delete(), 6000))}
+
+    } else if (inte.customId === 'guiaFortuna'){    // GUIA FORTUNA
+    
+        const selectChoice = inte.values[0];
+        const fs = require('fs');
+        const path = (`./src/embeds/guia/fortuna/${selectChoice}.json`);
+
+    if ((typeof(selectChoice) != 'undefined')&&(fs.existsSync(path))){
+
+        const embed = (new EmbedBuilder(require(`../src/embeds/guia/fortuna/${selectChoice}.json`))).data ;
+        const map = new Map(Object.entries(embed));
+
+        let embedcount = 0
+        for (let i=0;(i < map.size);i++){
+            embedcount = i
+        }
+
+        if (embedcount === 0){
+            inte.reply({embeds:[embed[0]], ephemeral : true}).then(msg => setTimeout(() => msg.delete(), 360000));
+        } else if (embedcount === 1){
+            inte.reply({embeds:[embed[0],embed[1]], ephemeral : true}).then(msg => setTimeout(() => msg.delete(), 360000));
+        } else if (embedcount === 2){
+            inte.reply({embeds:[embed[0],embed[1],embed[2]], ephemeral : true}).then(msg => setTimeout(() => msg.delete(), 360000)); 
+        } else if (embedcount === 3){
+            inte.reply({embeds:[embed[0],embed[1],embed[2],embed[3]], ephemeral : true}).then(msg => setTimeout(() => msg.delete(), 360000)); 
+        } else if (embedcount === 4){
+            inte.reply({embeds:[embed[0],embed[1],embed[2],embed[3],embed[4]], ephemeral : true}).then(msg => setTimeout(() => msg.delete(), 360000)); 
+        } else if (embedcount === 5){
+            inte.reply({embeds:[embed[0],embed[1],embed[2],embed[3],embed[4],embed[5]], ephemeral : true}).then(msg => setTimeout(() => msg.delete(), 360000)); 
+        } else if (embedcount === 6){
+            inte.reply({embeds:[embed[0],embed[1],embed[2],embed[3],embed[4],embed[5],embed[6]], ephemeral : true}).then(msg => setTimeout(() => msg.delete(), 360000)); 
+        } else if (embedcount === 7){
+            inte.reply({embeds:[embed[0],embed[1],embed[2],embed[3],embed[4],embed[5],embed[6],embed[7]], ephemeral : true}).then(msg => setTimeout(() => msg.delete(), 360000)); 
+        } else if (embedcount === 8){
+            inte.reply({embeds:[embed[0],embed[1],embed[2],embed[3],embed[4],embed[5],embed[6],embed[7],embed[8]], ephemeral : true}).then(msg => setTimeout(() => msg.delete(), 360000)); 
+        } else if (embedcount === 9){
+            inte.reply({embeds:[embed[0],embed[1],embed[2],embed[3],embed[4],embed[5],embed[6],embed[7],embed[8],embed[9]], ephemeral : true}).then(msg => setTimeout(() => msg.delete(), 360000)); 
+        } else { inte.reply({content:'esse guia possui mais de 9 embeds ... ❌',ephemeral:'true'}).then(msg => setTimeout(() => msg.delete(), 6000))}
+    } else { inte.reply({content:'essa guia ainda não está pronto ... 🙄',ephemeral:'true'}).then(msg => setTimeout(() => msg.delete(), 6000))}
 
     }    // GUIA ...
 })
